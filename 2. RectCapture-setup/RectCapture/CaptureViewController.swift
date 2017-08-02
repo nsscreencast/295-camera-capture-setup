7052e831a923f218abfdb4871d481a030bda3a08
Ben Scheirman <ben@scheirman.com>
2017-08-02 14:07:05 -0500

    Episode 295

---
+++ 2. RectCapture-setup/RectCapture/CaptureViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  RectCapture
+//
+//  Created by Ben Scheirman on 6/27/17.
+//  Copyright © 2017 NSScreencast. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import CoreImage
+
+class CaptureViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    lazy var captureSession: AVCaptureSession = {
+        let session = AVCaptureSession()
+        session.sessionPreset = .high
+        return session
+    }()
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if AVCaptureDevice.authorizationStatus(for: .video) == .authorized {
+            setupCaptureSession()
+        } else {
+            // prompt the user...
+        }
+    }
+    
+    // MARK: - Rotation
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return [.portrait]
+    }
+    
+    // MARK: - Camera Capture
+    
+    private func findCamera() -> AVCaptureDevice? {
+        let deviceTypes: [AVCaptureDevice.DeviceType] = [
+            .builtInDualCamera,
+            .builtInTelephotoCamera,
+            .builtInWideAngleCamera
+        ]
+        
+        let discovery = AVCaptureDevice.DiscoverySession(deviceTypes: deviceTypes,
+                                                         mediaType: .video,
+                                                         position: .back)
+        
+        return discovery.devices.first
+    }
+    
+    private func setupCaptureSession() {
+        guard captureSession.inputs.isEmpty else { return }
+        guard let camera = findCamera() else {
+            print("No camera found")
+            return
+        }
+        
+        do {
+            let cameraInput = try AVCaptureDeviceInput(device: camera)
+            captureSession.addInput(cameraInput)
+            
+            
+        } catch let e {
+            print("Error creating capture session: \(e)")
+            return
+        }
+    }
+    
+}
+
